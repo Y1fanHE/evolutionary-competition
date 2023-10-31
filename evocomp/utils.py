28771e83cfef2b09e9f167bc81b5a7581f5551fc
Y1fanHE <he.yifan.xs@alumni.tsukuba.ac.jp>
2023-10-31 10:21:17 +0800

    add creat_function_by_repetition

--- evocomp/utils.py
+++ evocomp/utils.py
@@ -26,7 +26,21 @@ def create_function(expression, n_var=2):
     return eval(func_str)
 
 
-def create_problem(expression, xl, xu, n_var):
+def create_function_by_repetition(expression, n_var=3):
+    func = create_function(expression)
+    var_str = ",".join([f"x{i}" for i in range(n_var)])
+    expr_str = "+".join([f"func(x{i},x{j})" for i, j in zip(range(n_var-1), range(1,n_var))])
+    func_str = f"lambda {var_str}: 1/{n_var-1}*({expr_str})"
+    return eval(func_str, {"func":func})
+
+
+def create_problem(expression, xl, xu, n_var, repeat=False):
+    if repeat:
+        return Problem(func=create_function_by_repetition(expression, n_var),
+                       xl=xl,
+                       xu=xu,
+                       n_var=n_var,
+                       input_mode="args")
     return Problem(func=create_function(expression, n_var),
                    xl=xl,
                    xu=xu,
